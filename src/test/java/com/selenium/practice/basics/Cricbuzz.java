026a3669ba61980dbb5d0d29d133f496130383e3
Ranjith-GIT <ranjithkumarrr.333@gmail.com>
2021-07-30 00:55:30 +0530

    new commit

---
+++ src/test/java/com/selenium/practice/basics/Cricbuzz.java
@@ -0,0 +1,30 @@
+package com.selenium.practice.basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Cricbuzz {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\driverExecutables\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.get("https://www.cricbuzz.com/");
+		Thread.sleep(4000);
+		System.out.println("Before Clicking");
+		driver.findElement(By.id("search_bar_menu")).sendKeys("Dravid");
+		Actions act = new Actions(driver);
+		act.sendKeys(Keys.ENTER).build().perform();
+		Thread.sleep(6000);
+		System.out.println("Print "+ driver.getTitle());
+		System.out.println("After Clicking");
+		driver.quit();
+		
+	}
+
+}
